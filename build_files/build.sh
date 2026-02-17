c9dfe5681399413aca9c7a3be98f7e9596f0e564
tohurtv <joshwebb84@outlook.com>
2026-02-17 08:05:30 -0700

    Change kernels

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,8 +4,16 @@ set -ouex pipefail
 
 ## Enable repos
 dnf5 -y install dnf5-plugins
+dnf5 -y copr enable bieszczaders/kernel-cachyos
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 dnf5 -y copr enable atim/xpadneo
+
+#remove fedora kernel
+dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
+
+# Install cachyos kernel
+dnf5 -y install kernel-cachyos kernel-cachyos-devel-matched
+
 
 # install packages
 dnf5 -y install ananicy-cpp \
@@ -14,10 +22,21 @@ dnf5 -y install ananicy-cpp \
   scx-tools \
   mangohud \
   mangohud.i686 \
-  xpadneo \
+  dkms \
   flatpak
 
 # enable flathub
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 
+#Build initramfs
+if [[ "${KERNEL_FLAVOR:-}" == "surface" ]]; then
+    KERNEL_SUFFIX="surface"
+else
+    KERNEL_SUFFIX=""
+fi
+
+QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel${KERNEL_SUFFIX:+-${KERNEL_SUFFIX}}")"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
