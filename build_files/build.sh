a2ee828cb1dfc5a6c5d770236ada992910966eff
tohurtv <joshwebb84@outlook.com>
2026-02-17 10:35:30 -0700

    fix

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,25 +8,6 @@ dnf5 -y copr enable bieszczaders/kernel-cachyos
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 dnf5 -y copr enable atim/xpadneo
 
-#remove fedora kernel
-dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
-
-# Install cachyos kernel
-dnf5 -y --setopt=tsflags=noscripts install kernel-cachyos kernel-cachyos-devel-matched
-setsebool -P domain_kernel_load_modules on
-
-#Build initramfs
-# Determine the installed CachyOS kernel version
-QUALIFIED_KERNEL=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-cachyos)
-
-# Generate module dependencies
-depmod "$QUALIFIED_KERNEL"
-
-# Generate initramfs for that kernel
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v \
-    --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
 
 # install packages
 dnf5 -y install ananicy-cpp \
