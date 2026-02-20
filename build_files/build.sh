47ca7d89f2125c89811e31fcd267705936e66de0
tohurtv <joshwebb84@outlook.com>
2026-02-19 18:27:51 -0700

    add bore-sysctl

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,6 +18,7 @@ dnf5 -y --setopt=tsflags=noscripts install kernel-cachyos kernel-cachyos-devel-m
 dnf5 -y install ananicy-cpp \
   cachyos-ananicy-rules \
   cachyos-settings \
+  bore-sysctl \
   scx-scheds \
   scx-tools \
   mangohud \
