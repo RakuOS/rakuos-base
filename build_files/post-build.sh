1a8a967cbf40db03d8e1c0b8baa5edd62b3b2a55
tohurtv <joshwebb84@outlook.com>
2026-03-12 04:24:29 -0600

    add manifest gen back

--- build_files/post-build.sh
+++ build_files/post-build.sh
@@ -1,1 +1,3 @@
 #sed -i '/ConditionPathExists=!\/run\/ostree-booted/d' /usr/lib/systemd/system/packagekit.service
+echo "Generating base file manifest..."
+/usr/libexec/rakuos/generate-base-manifest
