b7af36cd82b3e2210f51571192305d748cd53cda
tohurtv <joshwebb84@outlook.com>
2026-03-12 03:48:49 -0600

    fix

--- build_files/post-build.sh
+++ build_files/post-build.sh
@@ -1,3 +1,1 @@
 #sed -i '/ConditionPathExists=!\/run\/ostree-booted/d' /usr/lib/systemd/system/packagekit.service
-echo "Generating base file manifest..."
-/usr/libexec/rakuos/generate-base-manifest
