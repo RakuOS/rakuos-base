0113bc676c7776b7d0ac0f33acd32688dd682ccc
tohurtv <joshwebb84@outlook.com>
2026-03-22 03:20:00 -0600

    another change

--- build_files/build.sh
+++ build_files/build.sh
@@ -63,7 +63,7 @@ dnf5 -y --setopt=tsflags=noscripts install kernel-cachyos kernel-cachyos-devel-m
 
 dnf5 -y swap ffmpeg ffmpeg-free --allowerasing
 
-dnf5 -y install mesa-dri-drivers.i686 mesa-va-drivers.i686
+dnf5 -y install mesa-dri-drivers.i686 mesa-va-drivers.i686 mesa-vulkan-drivers.i686 mesa-libEGL.i686 mesa-libGL.i686 
 dnf5 -y upgrade --best 'mesa-*'
 
 #dnf5 -y swap mesa-va-drivers-freeworld mesa-va-drivers
