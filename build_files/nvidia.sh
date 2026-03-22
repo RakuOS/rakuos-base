3eb800b4b7ac530b4e4f0ff36574bacd5cae3af8
tohurtv <joshwebb84@outlook.com>
2026-03-21 20:56:20 -0600

    nvidia dkms fix

--- build_files/nvidia.sh
+++ build_files/nvidia.sh
@@ -2,29 +2,28 @@
 
 set -ouex pipefail
 
-# Install NVIDIA stack
+# Determine the installed kernel version
+QUALIFIED_KERNEL=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-cachyos)
+
+# Install NVIDIA stack + kernel devel headers needed for DKMS build
 dnf5 install -y \
     dkms-nvidia \
     nvidia-modprobe \
     nvidia-driver \
     nvidia-settings \
     nvidia-persistenced \
-    nvidia-settings \
-    libva-nvidia-driver
+    libva-nvidia-driver \
+    kernel-cachyos-devel-${QUALIFIED_KERNEL}
 
-#build akmods and update initramfs with nvidia drivers included
-KVER="$(dnf5 repoquery --installed --qf '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-cachyos)"
-#akmods --force --kernels "$KVER"
-
-#Build initramfs
-# Determine the installed kernel version
-QUALIFIED_KERNEL=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-cachyos)
+# Build DKMS module for the installed kernel
+NVIDIA_VER=$(rpm -q --queryformat '%{VERSION}\n' dkms-nvidia)
+dkms install -m nvidia -v "${NVIDIA_VER}" -k "${QUALIFIED_KERNEL}" --force
 
 # Generate module dependencies
-#depmod "$QUALIFIED_KERNEL"
+depmod "${QUALIFIED_KERNEL}"
 
-# Generate initramfs for that kernel
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v \
-    --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+# Generate initramfs with nvidia module included
+/usr/bin/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible --zstd -v \
+    --add ostree --add fido2 -f "/usr/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
 
-chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
+chmod 0600 /usr/lib/modules/"${QUALIFIED_KERNEL}"/initramfs.img
