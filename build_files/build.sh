0a02a623a4c7d4536604d3829551eae3da7df5e7
tohurtv <joshwebb84@outlook.com>
2026-04-15 19:42:44 -0600

    remove va drivers for now

--- build_files/build.sh
+++ build_files/build.sh
@@ -63,7 +63,8 @@ dnf5 -y --setopt=tsflags=noscripts install kernel-cachyos kernel-cachyos-devel-m
 
 dnf5 -y swap ffmpeg ffmpeg-free --allowerasing
 
-dnf5 -y install mesa-dri-drivers.i686 mesa-va-drivers.i686 mesa-vulkan-drivers.i686 mesa-libEGL.i686 mesa-libGL.i686
+#dnf5 -y install mesa-dri-drivers.i686 mesa-va-drivers.i686 mesa-vulkan-drivers.i686 mesa-libEGL.i686 mesa-libGL.i686
+dnf5 -y install mesa-dri-drivers.i686 mesa-vulkan-drivers.i686 mesa-libEGL.i686 mesa-libGL.i686
 #dnf5 -y upgrade --best 'mesa-*'
 
 # Determine the installed kernel version
